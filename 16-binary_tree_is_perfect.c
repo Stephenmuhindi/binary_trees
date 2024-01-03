40c936fcf260cfff9e9e7e71b174a0e81fb743a2
Stephenmuhindi <bteneson42@gmail.com>
2024-01-03 05:21:21 -0800

    taylor gang

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,75 @@
+#include "binary_trees.h"
+
+unsigned char is_leaf(const binary_tree_t *node);
+size_t depth(const binary_tree_t *tree);
+const binary_tree_t *get_leaf(const binary_tree_t *tree);
+int is_perfect_recursive(const binary_tree_t *tree,
+		size_t leaf_depth, size_t level);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+
+/**
+ * is_leaf - tucheki kama node ni leaf
+ * @node: pointer to the node tunacheki
+ * Return: leaf=1, else 0.
+ */
+unsigned char is_leaf(const binary_tree_t *node)
+{
+	return ((node->left == NULL && node->right == NULL) ? 1 : 0);
+}
+
+/**
+ * depth - Return depth
+ * @tree: pointer to root node ya tree tunacheki
+ * Return: depth else 0
+ */
+size_t depth(const binary_tree_t *tree)
+{
+	return (tree->parent != NULL ? 1 + depth(tree->parent) : 0);
+}
+
+/**
+ * get_leaf - Return leaf
+ * @tree: pointer to root node ya tree tunacheki
+ * Return: pointer to leaf ya kwanza
+ */
+const binary_tree_t *get_leaf(const binary_tree_t *tree)
+{
+	if (is_leaf(tree) == 1)
+		return (tree);
+	return (tree->left ? get_leaf(tree->left) : get_leaf(tree->right));
+}
+
+/**
+ * is_perfect_recursive - tunacheki kama ni perfect recursively.
+ * @tree: pointer to root node ya tree tunacheki
+ * @leaf_depth: depth ya hio leaf
+ * @level: level io node iko
+ *
+ * Return: perfect=1, else 0.
+ */
+int is_perfect_recursive(const binary_tree_t *tree,
+		size_t leaf_depth, size_t level)
+{
+	if (is_leaf(tree))
+		return (level == leaf_depth ? 1 : 0);
+	if (tree->left == NULL || tree->right == NULL)
+		return (0);
+	return (is_perfect_recursive(tree->left, leaf_depth, level + 1) &&
+		is_perfect_recursive(tree->right, leaf_depth, level + 1));
+}
+
+/**
+ * binary_tree_is_perfect - inacheki perfectness
+ * @tree: pointer to root node ya tree tunacheki
+ *
+ * description zaidi for samba: A perfect binary tree is both complete 
+ * filled from left to right on every level and full ie 0 or 2 cildren
+ *
+ * Return: NULL/ not perfect=0.else 1
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (is_perfect_recursive(tree, depth(get_leaf(tree)), 0));
+}
